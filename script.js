7dca38ee442b6370dcc352b5878cc1e9056e84e2
github username <github email address>
2017-01-19 15:03:57 -0500

    Done.

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  // Now we start the Ajax GET request. The first parameter is the URL with the data.
+  // The second parameter is a function that handles the response.
+  $.get("sentence.html", function(response) {
+    // Here we are getting the element on the page with the id of sentences and
+    // inserting the response
+    $("#sentences").html(response);
+  });
+});
